7633ee91e68401a55e09b2b6470011d3839b8740
Adrian <a.todd834@mybvc.ca>
2020-12-16 15:21:07 -0700

    -Budget reached event

--- Forms/BudgetForm.Designer.cs
+++ Forms/BudgetForm.Designer.cs
@@ -78,7 +78,7 @@
             this.label1.ForeColor = System.Drawing.Color.WhiteSmoke;
             this.label1.Location = new System.Drawing.Point(574, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.Size = new System.Drawing.Size(76, 22);
             this.label1.TabIndex = 2;
             this.label1.Text = "Sort By:";
             // 
@@ -129,7 +129,7 @@
             this.Controls.Add(this.panelMain);
             this.Controls.Add(this.panelTopBar);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "BudgetForm";
             this.Text = "Budget";
